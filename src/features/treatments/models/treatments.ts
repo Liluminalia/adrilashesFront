d45a9a1622f109b4518587818036ef90fe88b0be
Liluminalia <peloazul4@gmail.com>
2022-12-06 13:37:04 +0100

    adding models and carpet structure

---
+++ src/features/treatments/models/treatments.ts
@@ -0,0 +1,16 @@
+export type ProtoTreatmentI = {
+    title?: string;
+    img?: string;
+    description?: string;
+    price?: number;
+    time?: number;
+};
+
+export type TreatmentI = {
+    id: string;
+    title: string;
+    img: string;
+    description: string;
+    price: number;
+    time: number;
+};
---
+++ src/features/users/models/users.ts
@@ -0,0 +1,29 @@
+import { TreatmentI } from '../../treatments/models/treatments';
+
+export type ProtoUserI = {
+    name?: string;
+    email?: string;
+    password?: string;
+    phone?: string;
+    role?: 'admin' | 'user';
+    isVip?: boolean;
+    appointments?: Array<Appointment>;
+};
+
+export type Appointment = {
+    _id: TreatmentI;
+    treatmentId: TreatmentI;
+    date?: Date;
+    discount?: number;
+};
+
+export type UserI = {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    phone: string;
+    role: 'admin' | 'user';
+    isVip: boolean;
+    appointments: Array<Appointment>;
+};
