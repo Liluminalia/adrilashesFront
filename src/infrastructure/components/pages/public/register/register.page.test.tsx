5f2b28ad3d971f172c5b04e370697f2c750de11a
Liluminalia <peloazul4@gmail.com>
2022-12-13 16:38:10 +0100

    fixing code smells

--- src/infrastructure/components/pages/public/register/register.page.test.tsx
+++ src/infrastructure/components/pages/public/register/register.page.test.tsx
@@ -1,15 +1,12 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { MemoryRouter as Router } from 'react-router-dom';
-import { mockStoreAdmin } from '../../../../mockStore/mockStore';
-import { mockStoreUnlogged } from '../../../../mockStore/mockStoreUnlogged';
 import { appStore } from '../../../../store/store';
 import { RegisterPage } from './register.page';
-const mockNavigate = jest.fn();
 
+const mockNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
-    ...(jest.requireActual('react-router-dom') as any),
+    ...(jest.requireActual('react-router-dom') as jest.Mock),
     useNavigate: () => mockNavigate,
 }));
 describe('Given Register page', () => {
